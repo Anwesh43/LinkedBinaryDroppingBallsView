7d2c4ec7197cdb213cd096dcbeaa738ebc05b430
Anwesh43 <anweshthecool0@gmail.com>
2019-08-11 13:30:44 +0530

    adding constants and libraries

--- binarydroppingballsview/src/main/java/com/anwesh/uiprojects/binarydroppingballsview/BinaryDroppingBallsView.kt
+++ binarydroppingballsview/src/main/java/com/anwesh/uiprojects/binarydroppingballsview/BinaryDroppingBallsView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.binarydroppingballsview
 /**
  * Created by anweshmishra on 11/08/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.app.Activity
+import android.content.Context
+
+val nodes : Int = 5
+val parts : Int = 2
+val scGap : Float = 0.05f
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#4527A0")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val scDiv : Double = 0.51
+
