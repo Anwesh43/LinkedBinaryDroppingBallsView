6ef70f85a3b4e45b140eb159019fce9c42754855
Anwesh43 <anweshthecool0@gmail.com>
2019-08-11 13:52:17 +0530

    adding BDBNode class

--- binarydroppingballsview/src/main/java/com/anwesh/uiprojects/binarydroppingballsview/BinaryDroppingBallsView.kt
+++ binarydroppingballsview/src/main/java/com/anwesh/uiprojects/binarydroppingballsview/BinaryDroppingBallsView.kt
@@ -160,4 +160,27 @@ class BinaryDroppingBallsView(ctx : Context) : View(ctx)  {
             return this
         }
     }
+
+    data class BinaryDroppingBalls(var i : Int) {
+
+        private var curr : BDBNode = BDBNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
