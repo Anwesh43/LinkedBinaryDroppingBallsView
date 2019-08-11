a53da62e85e79516654e327088d08a522e59f942
Anwesh43 <anweshthecool0@gmail.com>
2019-08-11 13:50:44 +0530

    adding BDBNode class

--- binarydroppingballsview/src/main/java/com/anwesh/uiprojects/binarydroppingballsview/BinaryDroppingBallsView.kt
+++ binarydroppingballsview/src/main/java/com/anwesh/uiprojects/binarydroppingballsview/BinaryDroppingBallsView.kt
@@ -117,4 +117,47 @@ class BinaryDroppingBallsView(ctx : Context) : View(ctx)  {
             }
         }
     }
+
+    data class BDBNode(var i : Int, val state : State = State()) {
+
+        private var next : BDBNode? = null
+        private var prev : BDBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = BDBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBDBNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BDBNode {
+            var curr : BDBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
